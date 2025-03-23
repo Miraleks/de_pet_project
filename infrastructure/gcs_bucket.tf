ad48acf7f7c81d1d25b8ee327ef5d05de694a2bb
Alexey Mironositskiy <alexey@phantomrausch.de>
2025-03-23 21:08:33 +0100

    chore(infra): update terraform variables & gcs_bucket

---
+++ infrastructure/gcs_bucket.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "data_bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  storage_class = "STANDARD"
+
+  lifecycle_rule {
+    condition {
+      age = 365
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  uniform_bucket_level_access = true
+}
